4bf4dffe15e6ddaa48ab857dfdf92f5a8a9bbb5f
Loss Shirley <shirleyloss@LossdeMacBook-Pro.local>
2022-08-20 03:54:18 -0700

    Need to fix bug with prof.

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes/index');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const mongoSanitize = require("express-mongo-sanitize");
+const app = express();
+const expressSession = require('express-session');
+const Registration = mongoose.model('Registration');
+const passportLocal = require("passport-local"); //
+// const rateLimit = require("express-rate-limit");
+// const xss = require("xss-clean");
+// const helmet = require("helmet");
+// const limit = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many requests",
+// });
+
+const bcrypt = require("bcrypt");
+
+// app.use(helmet());
+// app.use(xss());
+// app.use(mongoSanitize());
+// app.use("/routeName", limit);
+// app.use(express.json({ limit: "10kb" }));
+// app.use(require("morgan")("combined"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static("public"));
+app.use(
+    expressSession({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1 * 60 * 1000,
+      },
+    })
+  );
+app.use("/", router);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(Registration.serializeUser()); //repeated
+passport.deserializeUser(Registration.deserializeUser()); //repeated
+  
+passport.use(
+  new passportLocal(function (isuserName, password, done) {
+    Registration.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
+      if (!user.verifyPassword(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
+  
+passport.serializeUser(Registration.serializeUser());
+passport.deserializeUser(Registration.deserializeUser());
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,110 @@
+const connectEnsureLogin = require('connect-ensure-login');
+const express = require("express");
+const mongoose = require('mongoose');
+const router = express.Router();
+const passport = require('passport');
+
+const bcrypt = require("bcrypt"); //Also in app.js
+const Registration = require('../models/Registration');
+
+const { check, validationResult } = require('express-validator');
+
+router.get('/', (req, res) => {
+  //res.send('It works!');
+  res.render('index', { title: 'home page', path: req.url });
+});
+
+router.get('/home', (req, res) => {
+  //res.send('It works!');
+  res.render('index', { title: 'home page', path: req.url });
+});
+
+router.get('/register', (req, res) => {
+  //res.send('It works!');
+  res.render('index', { title: 'register page', path: req.url });
+});
+
+router.get('/thankyou', (req, res) => {
+  //res.send('It works!');
+  res.render('index', { title: 'thankyou page', path: req.url, content: "Thank you for your registration!" });
+});
+
+router.get('/login', (req, res) => {
+  //res.send('It works!');
+  res.render('index', { title: 'login page', path: req.url });
+});
+
+router.get('/contact', 
+  connectEnsureLogin.ensureLoggedIn(),
+  function (req, res) {
+    res.render('index');
+  })
+
+// router.get('/registrants', basic.check((req, res) => {
+//   Registration.find()
+//     .then((registrations) => {
+//       res.render('registrants', { title: 'Listing registrations', registrations });
+//     })
+//     .catch(() => { 
+//       res.send('Sorry! Something went wrong.'); 
+//     });
+// }));
+
+router.post('/register', 
+    [
+      check('name')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a name'),
+      check('email')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+      check('username')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a username'),
+      check('password')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a password')
+    ],
+    (req, res) => {
+      //console.log(req.body);
+      const errors = validationResult(req);
+      if (errors.isEmpty()) {
+        Registration.register(
+          new Registration({
+            name: req.body.name,
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+          }),
+          req.body.password,
+          function (err, user) {
+            if (err) {
+              console.log(err);
+              res.rendder("register");
+            }
+            passport.authenticate("local")(req, res, function(){
+              res.redirect("/login");
+            });
+          }
+        );
+      } else {
+        res.render('index', { 
+            title: 'register page',
+            path: req.url,
+            errors: errors.array(),
+            data: req.body,
+        });
+      }
+    });
+
+router.post('/login', 
+  passport.authenticate('local', {
+    successRedirect: "/home",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+module.exports = router;
